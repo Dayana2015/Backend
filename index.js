5876bce30e9c6d7dcc3ba1396d5f1bac41b48371
Brillant-Minds-Grupo-5 <mosanz02@gmail.com>
2023-04-16 10:43:45 -0500

    v0.2

---
+++ index.js
@@ -0,0 +1,41 @@
+const express=require("express");
+
+const AdminEspecie=require("./AdminEspecie")
+class VeterinariaAPI{
+    constructor(){
+
+        this.puerto=3000;
+
+        this.app=express();
+
+        this.adminEspecie= new AdminEspecie();
+
+        this.app.post("/crear_especie", (req, res)=> {
+            this.adminEspecie.crearEspecie(req, res);
+        });
+
+        this.app.get("/listar_especie", (req, res)=>{
+            this.adminEspecie.listarEspecie(req, res);
+        });
+        
+        this.app.use(this.configurarCORS);
+        this.app.use(express.json());
+    }
+    configurarCORS(req, res , next){
+        res.header("Access-Control-Allow-Origin","*");
+        res.header("Access-Control-Allow-Methods","GET, POST");
+        req.header("Access-Control-Allow-Headers","Content-Type");
+        next();
+
+    }
+
+    iniciarServidor(){
+        this.app. listen(this.puerto, ()=>{
+            console.log(`servidor ejecuntandose en el puerto ${this.puerto}`);
+        });
+
+    }
+
+}
+const veterinariaAPI= new VeterinariaAPI();
+veterinariaAPI.iniciarServidor();
